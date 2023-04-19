2eb2c0a158c82818b07e4344380ad5a37cef1038
Shay ohana <91027583+shayohanaa@users.noreply.github.com>
2023-04-19 15:34:51 +0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
 
-driver = webdriver.Chrome(service=Service('<CHROMEDRIVER_PATH>'))
+driver = webdriver.Chrome(service=Service('C://Users/shayo/chromedriver_win32/chromedriver.exe'))
 driver.get("https://www.google.com/search?q=weather&oq")
 
 print("Current weather for Local Area:")
@@ -12,7 +12,6 @@ area = driver.find_element(By.CLASS_NAME, 'BBwThe').text
 wind = driver.find_element(By.ID, 'wob_ws').text
 humidity = driver.find_element(By.ID, 'wob_hm').text
 currentTime = driver.find_element(By.ID, 'wob_dts').text
-tomorrow = driver.find_element(By.XPATH, '//div/div/div[3]/div[3]/div/div[2]/div[1]').text
 minWeather = driver.find_element(By.XPATH, '//div[3]/div[3]/div/div[2]/div[3]/div[2]/span/span[1]').text
 maxWeather = driver.find_element(By.XPATH, '//div[3]/div[3]/div/div[2]/div[3]/div[1]/span/span[1]').text
 print('Current celsius:', celsius, '°C in', area)
@@ -22,11 +21,9 @@ print('Wind speed:', wind)
 print('Current time and day:', currentTime)
 print()
 driver.find_element(By.ID, 'dimg_3').click()
-celsius = driver.find_element(By.ID, 'wob_tm').text
-area = driver.find_element(By.CLASS_NAME, 'BBwThe').text
 wind = driver.find_element(By.ID, 'wob_ws').text
 humidity = driver.find_element(By.ID, 'wob_hm').text
-currentTime = driver.find_element(By.ID, 'wob_dts').text
+tomorrow = driver.find_element(By.XPATH, '//div/div/div[3]/div[3]/div/div[2]/div[1]').text
 minWeather = driver.find_element(By.XPATH, '//div[3]/div[3]/div/div[1]/div[3]/div[2]/span/span[1]').text
 maxWeather = driver.find_element(By.XPATH, '//div[3]/div[3]/div/div[1]/div[3]/div[1]/span/span[1]').text
 print('Weather for tomorrow:', '(', tomorrow, ')')
